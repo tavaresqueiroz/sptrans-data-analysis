62a0ef45c0c232567e0ca8b84e972c4fa36331fd
Victor Tavares <victor.tavares@itelios.com>
2017-07-11 02:33:00 -0300

    Changes.

--- download.rb
+++ download.rb
@@ -1,14 +1,61 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'net/http'
 
-page = Nokogiri::HTML(open("http://www.prefeitura.sp.gov.br/cidade/secretarias/transportes/institucional/sptrans/acesso_a_informacao/index.php?p=228269"))
-file = File.open("saida2.txt", 'a')
+module Download
 
-calendarios = page.css('.calendarios')
+    FULL_URL = "http://www.prefeitura.sp.gov.br/cidade/secretarias/transportes/institucional/sptrans/acesso_a_informacao/index.php?p=228269"
 
-puts calendarios.children.length
+    def Download.get_files
 
-calendarios.children.each { |calendario|
-    file.write(calendario.css('table'))
-}
+        page = Nokogiri::HTML(open(FULL_URL))
+        
+        calendarios = page.css('.calendarios')
+
+        puts "Download dos arquivos:\n"
+
+        calendarios.children.each { |calendario|
+            
+            caption = calendario.css('caption')
+            next if caption.text.strip.empty?
+
+            folder_path = File.join(Dir.pwd, 'months', caption.text.strip)
+            
+            unless File.directory?(folder_path)
+                Dir.mkdir(folder_path, 0777)
+            end
+
+            puts caption.text.strip
+
+            tbody = calendario.css('table').css('tbody')
+            tbody.css('td').each { |td|
+                anchor = td.css('a')
+                next if anchor.nil?
+                next if anchor[0].nil?
+                href = anchor[0]['href']
+                
+                file_name = href.split('/').last
+                
+                uri = URI(href)
+                
+                Net::HTTP.start(uri.host) do |http|
+                    unless File.exist?(File.join(folder_path, file_name))
+                        xls = File.open(File.join(folder_path, file_name), "wb")
+                        begin
+                            http.request_get(uri.path) do |resp|
+                                resp.read_body do |segment|
+                                    xls.write(segment)
+                                end
+                            end
+                        ensure
+                            xls.close()
+                        end
+                    end
+                end
+                puts "* " + file_name
+            }
+        }
+        puts "\n\n"
+    end
+end
--- main.rb
+++ main.rb
@@ -1,13 +1,56 @@
+$LOAD_PATH << '.'
+
 require 'spreadsheet'
+require 'download'
 
-workbook = Spreadsheet.open (File.join(Dir.pwd, "files", "Passag-20170101.xls"))
-worksheet = workbook.worksheets[0]
+#Download.get_files
 
-file = File.open("saida.txt", 'a')
+MAIN_FOLDER = 'months'
 
-#http://www.prefeitura.sp.gov.br/cidade/secretarias/transportes/institucional/sptrans/acesso_a_informacao/index.php?p=228269
+puts "Processando arquivos:"
 
-worksheet.each do |row|
-	break if row[0].nil?
-	file.write("#{row[4]} \t\t\t\t\ #{row[18]}\n") if row[3] == "SAMBAIBA"
+def process_xls(workbook, sheet)
+	
+	worksheet = workbook.worksheets[0]
+	
+	index = 0;
+	worksheet.each do |row|
+		break if row[0].nil?
+		next unless row[3] == "SAMBAIBA"
+		
+		column = sheet.row(index).column(0) 
+		
+
+		sheet.row(index).push(row[4]) unless row[4].nil? && sheet.row(index) == row[4]
+		sheet.row(index).push(row[18]) unless row[18].nil?
+
+		index += 1
+	end
+
+	return true
+end
+
+Dir.foreach(File.join(Dir.pwd, MAIN_FOLDER)) do |month|
+	next if month == '.' || month == '..'
+	
+	puts month
+
+	newbook = Spreadsheet::Workbook.new
+	sheet = newbook.create_worksheet
+
+	subfolder = File.join(Dir.pwd, MAIN_FOLDER, month)
+
+	can_write = false
+
+	Dir.foreach(subfolder) do |xls|
+		next if xls == '.' || xls == '..' || month + ".xls" == xls
+		
+		puts "* " + xls
+
+		workbook = Spreadsheet.open(File.join(Dir.pwd, MAIN_FOLDER, month, xls))
+		can_write = process_xls(workbook, sheet)
+	end
+
+	newbook.write(File.join(subfolder, month + ".xls")) if can_write
+
 end
